38f0f1b5406cf528ba3a64101a673a5f50c05aea
pt <ptsghs@163.com>
2022-11-07 18:56:43 +0800

    add demo

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
   // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
   publicPath: '/minio',
   // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
-  outputDir: 'dist',
+  outputDir: 'minio',
   // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
   assetsDir: 'static',
   chainWebpack(config) {
